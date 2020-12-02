755104eedc64550c66c972c9cb5c856dcbfe182f
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-12-02 00:25:29 -0600

    part 2

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+
+    post                    = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name                    = models.CharField(max_length=50)
+    email                   = models.EmailField()
+    content                 = models.TextField()
+    publish                 = models.DateTimeField(auto_now_add=True)
+    status                  = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('publish',)
+
+    def __str__(self):
+        return f'Comment by {self.name}'
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 
-from .models import Post
+from .models import Post, Comment
+from .forms import NewCommentForm
 
 def home(request):
 
@@ -12,4 +13,27 @@ def post_single(request, post):
 
     post = get_object_or_404(Post, slug=post, status='published')
 
-    return render(request, 'single.html', {'post': post})
+    comments = post.comments.filter(status=True)
+
+    user_comment = None
+
+    if request.method == 'POST':
+        comment_form = NewCommentForm(request.POST)
+
+        if comment_form.is_valid():
+            user_comment = comment_form.save(commit=False)
+            user_comment.post = post
+            user_comment.save()
+            return HttpResponseRedirect("/" + post.slug)
+        else:
+            comment_form = NewCommentForm()
+        return render(
+            request,
+            'single.html',
+            {
+                'post': post,
+                'user_comment': user_comment,
+                'comments': comments,
+                'comment_form': comment_form,
+            },
+        )
